51dcb14d227a810c6079a803e92a02dd099df0e3
Gideon Idoko <iamgideonidoko@gmail.com>
2024-04-16 04:42:18 +0100

    chore: base folder arch in src dir

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '@/App';
 
 // NOTE: import explicitly to use the types shipped with jest.
 import { it } from '@jest/globals';
