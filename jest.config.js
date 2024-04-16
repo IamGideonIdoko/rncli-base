2d5f7835bf1ffe77f1ae4eb5843ae5eff20b2468
Gideon Idoko <iamgideonidoko@gmail.com>
2024-04-16 04:27:07 +0100

    chore: configure jest for tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,31 @@
+/* eslint-disable no-comments/disallowComments */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { pathsToModuleNameMapper } = require('ts-jest');
+require('ts-jest/presets');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js', './src/jest.setup.js'],
+  transform: {
+    '^.+\\.jsx$': 'babel-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  //   transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation|@notifee/react-native)',
+  ],
+  modulePaths: ['./src'],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper({
+      '@/*': ['*'],
+    }),
+    '\\.svg': '<rootDir>/src/__mocks__/svg.mock.js',
+  },
 };
